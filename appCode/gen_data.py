222a8153cfbf6ec2c01ce7324d05d282aa8caf01
Manas Joglekar <brahmaneya@gmail.com>
2015-04-30 12:01:27 -0700

    deleted some big files

--- appCode/gen_data.py
+++ appCode/gen_data.py
@@ -4,8 +4,8 @@ import sys,os
 import random
 import numpy as np 
 
-initializationTuples = 200020000 # tuples used for prior of beta distribution
-N = 20000 + initializationTuples # total number of tuples
+initializationTuples = 0 # tuples used for prior of beta distribution
+N = 40000 + initializationTuples # total number of tuples
 pTrainPos = 0.2 # probability that a positive tuple is in training data
 pTrainNeg = 0.2 # probability that a negative tuple is in training data
 T_p = 0.5 # base tuple truth rate
