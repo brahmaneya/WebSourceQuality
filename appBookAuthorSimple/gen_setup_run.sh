4f4340856cd6b4a7b759a0830cb9465788300b3c
Manas Joglekar <brahmaneya@gmail.com>
2015-07-22 18:39:49 -0700

    small change

--- appBookAuthorSimple/gen_setup_run.sh
+++ appBookAuthorSimple/gen_setup_run.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash 
-rm -r ~/deepdive/out/
-python gen_data.py
+rm -r run/
 deepdive initdb
 ./setup_database.sh #Loads data into tables
 deepdive run
 deepdive sql "select * from dd_inference_result_weights_mapping order by description"
 deepdive sql "select bucket, is_true, count(*) from tuple_truth_is_true_inference_bucketed where is_true IS NOT NULL group by bucket, is_true order by bucket, is_true"
-deepdive sql "select bucket, is_true, count(*) from group_tuple_belief_is_true_inference_bucketed where is_true IS NOT NULL group by bucket, is_true order by bucket, is_true"
-#$PSQL "select bucket, is_true, count(*) from source_group_tuple_belief_is_true_inference_bucketed where is_true IS NOT NULL group by bucket, is_true order by bucket, is_true"
