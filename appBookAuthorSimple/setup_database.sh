89e0d4c6bbd5881ff5c7cd404eb366cde029efed
Manas Joglekar <brahmaneya@gmail.com>
2015-07-22 19:26:13 -0700

    small changes

--- appBookAuthorSimple/setup_database.sh
+++ appBookAuthorSimple/setup_database.sh
@@ -11,6 +11,6 @@ fi
 
 export APP_HOME=`cd $(dirname $0)/; pwd`
 
-deepdive sql "copy source_outputs_input from STDIN CSV;" < $APP_HOME/data/source_book_author.csv
-deepdive sql "copy tuples_input from STDIN CSV;" < $APP_HOME/data/book_author.csv
-deepdive sql "copy labeled_tuples_input from STDIN CSV;" < $APP_HOME/data/labeled_book_author.csv
+deepdive sql "copy source_outputs_input from STDIN CSV;" < $APP_HOME/data/srcBookAuthor.csv
+deepdive sql "copy tuples_input from STDIN CSV;" < $APP_HOME/data/bookAuthor.csv
+deepdive sql "copy labeled_tuples_input from STDIN CSV;" < $APP_HOME/data/bookAuthorTrue.csv
