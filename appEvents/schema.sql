9c00a7343706888df7dc4ecb3b83ded8e2d3545c
thodrek <thodrek@stanford.edu>
2015-11-04 13:43:00 -0800

    updated files

--- appEvents/schema.sql
+++ appEvents/schema.sql
@@ -2,6 +2,7 @@ CREATE EXTENSION postgis;
 
 DROP TABLE IF EXISTS gdelt_raw_input CASCADE;
 CREATE TABLE gdelt_raw_input(
+  eventid int,
   sqldate int,
   actor1name varchar(100),
   actor2name varchar(100),
@@ -11,14 +12,14 @@ CREATE TABLE gdelt_raw_input(
   actiongeo_countrycode varchar(10),
   actiongeo_lat real,
   actiongeo_long real,
-  sourceurl varchar(200),
+  sourceurl text,
   sourcedomain varchar(100),
   the_geom geometry,
   CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2),
   CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL),
   CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 4326)
-  )
-  DISTRIBUTED BY (sqldate);
+);
+
 
 
 DROP INDEX IF EXISTS gdelt_the_geom_gist CASCADE;
@@ -28,22 +29,26 @@ USING gist (the_geom);
 
 DROP TABLE IF EXISTS acled_raw_input CASCADE;
 CREATE TABLE acled_raw_input(
+  eventid varchar(100),
   eventdate int,
   event_type varchar(100),
-  country varchar(20),
-  location varchar(100),
+  country varchar(100),
   latitude real,
   longitude real,
   the_geom geometry,
   CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2),
   CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL),
   CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 4326)
-  )
-  DISTRIBUTED BY (eventdate);
+);
 
 
 DROP INDEX IF EXISTS acled_the_geom_gist CASCADE;
 CREATE INDEX aclead_the_geom_gist ON acled_raw_input
 USING gist (the_geom);
 
+DROP TABLE IF EXISTS african_countries CASCADE;
+CREATE TABLE african_countries(
+	fipscode varchar(2),
+	othercode varchar(10),
+	name text);
 
--- appEvents/update_database.sh
+++ appEvents/update_database.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 
-deepdive sql "update gdelt_input set the_geom = ST_GeomFromText('POINT('|| actiongeo_long || ' ' || actiongeo_lat || ')',4326)";
+deepdive sql "update gdelt_raw_input set the_geom = ST_GeomFromText('POINT('|| actiongeo_long || ' ' || actiongeo_lat || ')',4326)";
+deepdive sql "update acled_raw_input set the_geom = ST_GeomFromText('POINT('|| longitude || ' ' || latitude || ')',4326)";
