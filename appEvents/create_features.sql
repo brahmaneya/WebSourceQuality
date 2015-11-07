af55be7095ebb1ec5a72344c441ee4f053b667d5
Theo Rekatsinas <thodrek@raiders5.stanford.edu>
2015-11-07 10:31:14 -0800

    updated feature process

--- appEvents/create_features.sql
+++ appEvents/create_features.sql
@@ -19,3 +19,18 @@ select sourcedomain, count(CASE WHEN numsources < 50 THEN 1 ELSE null END) unpop
 count(CASE WHEN numsources >= 50 THEN 1 ELSE null END) popEvents, count(*) totalEvents
 from gdelt_raw_input
 group by sourcedomain;
+
+
+copy (
+select sourcedomain, 'SourceSize='||round((totalevents::float/37842::float)::numeric,3) as feat from sourceeventbreakdown
+where sourcedomain in (select * from srcwithfeats)
+UNION
+select sourcedomain, 'PopToUnpopEvents='||round((popevents::float/unpopevents::float)::numeric,3) as feat from sourceeventbreakdown
+where sourcedomain in (select * from srcwithfeats)
+UNION
+select sourcedomain, 'EventTypes='||eventtypes as feat from sourceeventtypes
+where sourcedomain in (select * from srcwithfeats)
+UNION
+select sourcedomain, 'Countries='||countries_touched as feat from sourcecountries
+where sourcedomain in (select * from srcwithfeats)
+) To '/tmp/srcMentionFeats.csv' With CSV;
--- appEvents/preprocess_with_affine.py
+++ appEvents/preprocess_with_affine.py
@@ -3,8 +3,8 @@ import random
 from sets import Set
 import urllib2
 import sympy
+import cPickle as pickle
 
-srcFeatures = {}
 featureValues = {}
 featureValues['Rank'] = []
 featureValues['Country Rank'] = []
@@ -17,128 +17,19 @@ featureValues['Total Sites Linking In'] = []
 maxF = {}
 minF = {}
 
-def getFeatures(srcId, srcName):
-    siteString = "http://www.alexa.com/siteinfo/";
-    urlString = siteString + srcName
-    try:
-        response = urllib2.urlopen(urlString)
-        page = response.read()
-        pageLines = page.split("\n")
-        totalLines = len(pageLines)
-        lidx = 0
-        while True:
-            inputLine = pageLines[lidx]
+# Read sources and src features
+sources = pickle.load(open(sys.argv[1],'r'))
+srcFeatures = pickle.load(open(sys.argv[2],'r'))
 
-            # Rank
-            if inputLine.find("Global rank icon") != -1:
-                lidx += 2
-                inputLine = pageLines[lidx]
-                rank = inputLine[:inputLine.index('<')]
-                rank = rank.replace(',','')
-                srcFeatures[srcId]['Rank'] = int(rank)
-                featureValues['Rank'].append(int(rank))
-
-            # Rank in Country
-            if inputLine.find("The rank by country is calculated using a combination") != -1:
-                lidx += 9
-                inputLine = pageLines[lidx]
-                cRank = inputLine[:inputLine.index('<')]
-                cRank = cRank.replace(',','')
-                srcFeatures[srcId]['Country Rank'] = int(cRank)
-                featureValues['Country Rank'].append(int(cRank))
-
-            # Country
-            #if inputLine.find("<strong>Estimated Pageviews</strong>") != -1:
-            #    lidx += 8
-            #    inputLine = pageLines[lidx]
-            #    country = inputLine[inputLine.index("<span class='text-inline'>")+len("<span class='text-inline'>"):inputLine.index("</span></span></td>")]
-            #    srcFeatures[srcId]['Country'] = country
-
-            # New Country
-            if inputLine.find('<span class="countryRank">') != -1:
-                lidx += 2
-                inputLine = pageLines[lidx]
-                country = inputLine[inputLine.index("title='")+len("title='"):inputLine.index("'>")]
-                srcFeatures[srcId]['Country'] = country
-                featureValues['Country'].append(country)
-
-            # Bounce rate
-            if inputLine.find("Bounce Rate</h4>") != -1:
-                lidx += 3
-                inputLine = pageLines[lidx]
-                bounceRate = inputLine[:inputLine.index("%")]
-                bounceRate = bounceRate.replace(',','')
-                srcFeatures[srcId]['Bounce Rate'] = float(bounceRate)
-                featureValues['Bounce Rate'].append(float(bounceRate))
-
-            # Daily pageviews per visitor
-            if inputLine.find("Daily Pageviews per Visitor</h4>") != -1:
-                lidx += 3
-                inputLine = pageLines[lidx]
-                pageViews = inputLine[:inputLine.index("<")]
-                pageViews = pageViews.replace(',','')
-                srcFeatures[srcId]['Daily Page Views Per Visitor'] = float(pageViews)
-                featureValues['Daily Page Views Per Visitor'].append(float(pageViews))
-
-            # Daily time on site per visitor
-            if inputLine.find("Daily Time on Site</h4>") != -1:
-                lidx += 3
-                inputLine = pageLines[lidx]
-                time = inputLine[:inputLine.index("<")]
-                time = time.replace(',','')
-                time = time.split(':')
-                totalTime = int(time[0])*60 + int(time[1])
-                srcFeatures[srcId]['Daily Time on Site'] = totalTime
-                featureValues['Daily Time on Site'].append(totalTime)
-
-            # Search Visits
-            if inputLine.find("Search Visits</h4>") != -1:
-                lidx += 3
-                inputLine = pageLines[lidx]
-                visits = inputLine[:inputLine.index("%")]
-                visits = visits.replace(',','')
-                srcFeatures[srcId]['Search Visits'] = float(visits)
-                featureValues['Search Visits'].append(float(visits))
-
-            # Total Sites Linking In
-            if inputLine.find("Total Sites Linking In</h5>") != -1:
-                lidx += 1
-                inputLine = pageLines[lidx]
-                links = inputLine[inputLine.index(">")+1:inputLine.index("</span")]
-                links = links.replace(',','')
-                srcFeatures[srcId]['Total Sites Linking In'] = int(links)
-                featureValues['Total Sites Linking In'].append(int(links))
-
-            lidx += 1
-            if lidx >= totalLines:
-                break
-
-        return 0
-    except:
-	print "Unexpected error:", sys.exc_info()[0]
-        return -1
-
-
-
-# Read sources and extract features
-sources = {}
-sFile = open('data/sources.csv','r')
-for l in sFile.readlines():
-    l = l.rstrip("\n")
-    print l
-    # l[0] -> id in raw dataset
-    # l[1] -> website for alexa crawling
-    srcFeatures[l] = {}
-    status = getFeatures(l,l)
-    if status == 0:
-        sources[l] = l
-
+for src in srcFeatures:
+	for f in srcFeatures[src]:
+		featureValues[f].append(srcFeatures[src][f])
 
 # Examine features
 for f in featureValues:
-    if f != 'Country':
-        maxF[f] = max(featureValues[f])
-        minF[f] = min(featureValues[f])
+	if f != 'Country':
+        	maxF[f] = max(featureValues[f])
+        	minF[f] = min(featureValues[f])
 
 # Preprocess source features
 allFeatures = {}
@@ -147,11 +38,11 @@ featureId = 0
 for src in sources:
     for k in srcFeatures[src]: 
         if k not in ['Country']:
-            fValue = srcFeatures[src][k]#ound((srcFeatures[src][k] - minF[k])*5/(maxF[k] - minF[k]))
+            fValue = round((srcFeatures[src][k] - minF[k])*30/(maxF[k] - minF[k]))
 	    if k not in allFeatures:
-		allFeatures[k] = featureId
-		id2Feature[featureId] = k
-		featureId += 1
+		  allFeatures[k] = featureId
+		  id2Feature[featureId] = k
+		  featureId += 1
 	    srcFeatures[src][k] = fValue
 	else:
             fValue = srcFeatures[src][k]
