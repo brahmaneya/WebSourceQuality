5dce6f16d5ab87d90cc169c86b46fb6b346066a5
Theo Rekatsinas <thodrek@raiders5.stanford.edu>
2015-11-07 12:05:58 -0800

    schema updated

--- appEvents/schema.sql
+++ appEvents/schema.sql
@@ -1,58 +1,38 @@
-CREATE EXTENSION postgis;
+DROP TABLE IF EXISTS eventPosVotes CASCADE;
+CREATE TABLE eventPosVotes(
+  source_id varchar(200),
+  geid int
+)
+DISTRIBUTED BY (geid);
 
-DROP TABLE IF EXISTS gdelt_raw_input CASCADE;
-CREATE TABLE gdelt_raw_input(
-  eventid int,
-  sqldate int,
-  actor1name varchar(100),
-  actor2name varchar(100),
-  eventcode varchar(10),
-  eventbasecode varchar(10),
-  eventrootcode varchar(10),
-  nummentions int,
-  numsources int,
-  numarticles int,
-  avgtone real,
-  actiongeo_countrycode varchar(10),
-  actiongeo_lat real,
-  actiongeo_long real,
-  sourceurl text,
-  sourcedomain varchar(100),
-  the_geom geometry,
-  CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2),
-  CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL),
-  CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 4326)
-);
+DROP TABLE IF EXISTS eventNegVotes CASCADE;
+CREATE TABLE eventNegVotes(
+  source_id varchar(200),
+  geid int
+)
+DISTRIBUTED BY (geid);
 
 
+DROP TABLE IF EXISTS source_features CASCADE;
+CREATE TABLE source_features(
+  source_id varchar(200),
+  feature text
+  )
+DISTRIBUTED BY (source_id);
 
-DROP INDEX IF EXISTS gdelt_the_geom_gist CASCADE;
-CREATE INDEX gdelt_the_geom_gist ON gdelt_raw_input
-USING gist (the_geom);
+DROP TABLE IF EXISTS event_truth CASCADE;
+CREATE TABLE event_truth(
+  geid int,
+  is_true boolean, -- whether tuple is true
+  id bigint -- reserved for deepdive
+  )
+DISTRIBUTED BY (geid);
 
-
-DROP TABLE IF EXISTS acled_raw_input CASCADE;
-CREATE TABLE acled_raw_input(
-  eventid varchar(100),
-  eventdate int,
-  event_type varchar(100),
-  country varchar(100),
-  latitude real,
-  longitude real,
-  the_geom geometry,
-  CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2),
-  CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL),
-  CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 4326)
-);
-
-
-DROP INDEX IF EXISTS acled_the_geom_gist CASCADE;
-CREATE INDEX aclead_the_geom_gist ON acled_raw_input
-USING gist (the_geom);
-
-DROP TABLE IF EXISTS african_countries CASCADE;
-CREATE TABLE african_countries(
-	fipscode varchar(2),
-	othercode varchar(10),
-	name text);
-
+DROP TABLE IF EXISTS source_event_truth CASCADE;
+CREATE TABLE source_event_truth(
+  source_id varchar(200),
+  geid int,
+  is_true boolean,
+  id bigint
+)
+DISTRIBUTED BY (geid);
